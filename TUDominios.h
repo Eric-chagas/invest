9d403e9a4656a60be42d81eeda115a7b60b1fa1d
Eric-chagas <eric_chagas12@outlook.com>
2020-09-28 15:37:25 -0300

    Iniciando classes de teste de unidade para os dominios

---
+++ TUDominios.h
@@ -0,0 +1,21 @@
+#ifndef TUDOMINIOS_H_INCLUDED
+#define TUDOMINIOS_H_INCLUDED
+
+//Classes teste que possuem metodos que testam os metodos publicos e lancamento de excecao
+//Testes sao realizados utilizando:
+//1. Ponteiro para objeto da classe em teste;
+//2. Valores constantes para casos de teste (falha e sucesso)
+//3. Metodos que preparam o ambiente de teste, testam, e limpam o ambiente novamente.
+//4. Os requisitos atendidos pelos testes, englobam os casos de uso especificados no documento com as instrucoes e requisitos
+//sobre o sistema desenvolvido.
+
+
+//CEP
+
+class TUCep{
+private:
+
+public:
+};
+
+#endif // TUDOMINIOS_H_INCLUDED
